e910fa5dc516b98258116226ca9440c469905a95
Mark KE <mark45397@gmail.com>
2023-07-05 15:49:16 +0800

    First Page

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
+import App from "./App.vue";
+import zh from "./language/zh-TW.json";
+import en from "./language/en-US.json";
+import ja from "./language/ja-JP.json";
+const i18n = createI18n({
+  legacy: false,
+  locale: "zh-TW",
+  fallbackLocale: "zh-TW",
+  messages: {
+    "zh-TW": zh,
+    "en-US": en,
+    "ja-JP": ja
+  }
+});
+createApp(App).use(i18n).mount("#app");
